df3c6f780d88a836681ecc71c624b574a31b2109
Pedro Martin <pataruco@gmail.com>
2015-05-11 19:30:28 +0100

    Create an empty array and the wireframes

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,20 @@
 console.log('lauren');
+
+$(document).ready(function(){
+  console.log('pedro')
+
+  var game = ("", "", "", "", "", "", "", "", "");
+
+
+
+
+
+  // Event listener function
+  
+
+
+
+
+
+
+}) //document. ready
