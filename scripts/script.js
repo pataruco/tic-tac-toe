b596f855e02cb759022275dd835d0fa7c4cd49a4
Pedro Martin <pataruco@gmail.com>
2015-05-12 11:38:36 +0100

    Getting the id of the slot clicked

--- scripts/script.js
+++ scripts/script.js
@@ -1,15 +1,16 @@
 $(document).ready(function(){
   console.log('pedro')
 
-  var game = ("", "", "", "", "", "", "", "", "");
+  var game = ["", "", "", "", "", "", "", "", ""];
   var equix = 'X';
   var cero = 'O';
 
 // function to add an X or 0 in each slot
 
 
-  function getClickInArray () {
-    
+  function getClickInArray(e) {
+    console.log(e.currentTarget.id);
+
   } //getClickInArray
 
 //Event listener  for the function getClickInArray
