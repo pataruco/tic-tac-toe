26fdfc5aac8aaa517020561b776577583c22a28b
Pedro Martin <pataruco@gmail.com>
2015-05-12 12:41:43 +0100

    Eventlistner input a X on an array

--- scripts/script.js
+++ scripts/script.js
@@ -1,15 +1,20 @@
 $(document).ready(function(){
   console.log('pedro')
 
-  var game = ["", "", "", "", "", "", "", "", ""];
+  var game = [null, null, null, null, null, null, null, null, null];
   var equix = 'X';
   var cero = 'O';
 
-// function to add an X or 0 in each slot
 
+// function to add an X or 0 in each slot
+/***************************************/
 
   function getClickInArray(e) {
-    console.log(e.currentTarget.id);
+    // console.log(e.currentTarget.id);
+    // console.log(game);
+    var inputArray = (e.currentTarget.id);
+    return game[inputArray] = equix;
+     // console.log(game);
 
   } //getClickInArray
 
