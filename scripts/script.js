1e8d418937cdabb5a74acdca8659c9c52e481106
Pedro Martin <pataruco@gmail.com>
2015-05-12 15:02:42 +0100

    making a function to alternate players

--- scripts/script.js
+++ scripts/script.js
@@ -1,9 +1,15 @@
 $(document).ready(function(){
   console.log('pedro')
 
+  /*Variables
+  *************************************/
+
   var game = [null, null, null, null, null, null, null, null, null];
   var equix = 'X';
   var cero = 'O';
+  var answers = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]];
+  var lastMove = 'X'
+  
 
 
 // function to add an X or 0 in each slot
@@ -11,19 +17,53 @@ $(document).ready(function(){
 
   function getClickInArray(e) {
     // console.log(e.currentTarget.id);
-    // console.log(game);
+    console.log(game);
     var inputArray = (e.currentTarget.id);
+    // checkWin();
     return game[inputArray] = equix;
-     // console.log(game);
+
+    
 
   } //getClickInArray
 
 //Event listener  for the function getClickInArray
   $('.slot').on("click", getClickInArray);
+  /******************************************/
+
+/*
+
+
+
+
+
+
+
+
+
+
+
+
+
+  // /*function to check proof win
+  // // ****************************************/
+  // function checkWin () {
+  //   $.each(answers, function(index, element) {
+  //     if(game === element){
+  //       alert('win');
+  //     }
+  //     else {
+  //       alert('draw');
+  //     }
+      
+  //     // console.log(index, element);
+  //   });
+  //   } //checkWin 
 
 
   
 
+
+  
 
 
 
